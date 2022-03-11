e0485abd3764a63151dca9f4388e8c54a34ab906
Lucas de Souza Paiva <lukas.souza.paiva.9@gmail.com>
2022-03-11 20:26:20 -0300

    :arrow_up: Docs update
    Colocando algumas fontes e detalhando um pouco como utilizar o script.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# report-SO-HW
-Shell script feito em bash para coleta de informações do sistema operacioanal e do hardware.
+<h1 align="center">Machine Information</h1>
+
+> Em bash, elaborar um shell script para coletar as principais informações do SO e também do hardware (nome da máquina, tamanho do disco, quantidade de RAM, processador, número de núcleos, velocidade, endereço de hardware da interface de rede, dispositivos usb, etc.)
+
+> Apresentar as informações formatadas na saída padrão (terminal de texto)<br>
+> Na tarefa, encaminhar:
+
+> -   O arquivo do shell script (comentário com a identificação dos integrantes)
+> -   Um printscreen da saída da execução do script
+> -   Incluir comentários sobre os recursos utilizados
+
+Shell script feito em bash para coleta de informações da máquina do sistema operacional e do hardware.
+
+Algumas possíveis referências:
+
+-   [40 comandos para coletar informações do sistema](#https://www.linuxdescomplicado.com.br/2015/11/top-40-comandos-coletar-informacoes-sistema.html) (Ubuntu).
+-   [Comandos para consultar informações de hardware no linux](#https://telazul.drusian.com.br/pt/artigo/comandos-para-consultar-informacoes-de-hardware-no-linux).
--- script.sh
+++ script.sh
@@ -1,36 +1,44 @@
 #!/bin/bash
+
+# Para utilizar alguns comandos será necessário a instalação das seguintes ferramentas:
+# Instalar net-tools por esse comando: sudo apt install net-tools
+# Instalar hwinfo por esse comando:    sudo apt install hwinfo
+
+# Erro que pode ocorrer no Windowns: "/bin/bash^M: bad interpreter: No such file or directory"
+# para resolver, utilizar esse comando no terminal: sed -i -e 's/\r$//' script.sh
+
 MEM="$(cat /proc/meminfo | grep "\MemTotal" | cut -d\: -f2-)"
- 
 MEM="$(echo ${MEM})"
 
 # Coleta info processador - quantidade
  
 NPROC="$(cat /proc/cpuinfo | grep -i processor | wc -l)"
- 
 NPROC="$(echo ${NPROC})" 
  
 # Coleta info processador - modelo
  
 PROC="$(cat /proc/cpuinfo | grep "\model name" | tail -1 | cut -d\: -f2-)"
- 
 PROC="$(echo ${PROC})" 
  
 # Coleta info discos
- 
-DISK=$(sudo fdisk -l | grep Disco | egrep -v "Identificador" | cut -d ' ' -f2-4 | cut -d ',' -f1)
+# Ocorre um problema quando utilizo no WSL, retorna todas as partições do disco, ver como "pular linha" a cada disco encontrado
+# ficaria melhor unindo os dados do disco, tanto o nome do disco e seu tamanho e aquele /dev/alguma_coisa.
+
+DISK=$(sudo fdisk -l | grep Disk | egrep -v "Virtual" | cut -d ' ' -f2-4 | cut -d ',' -f1)
 DISK="$(echo ${DISK})"
+
+# Coleta do nome do disco
  
-DISK_NAME=$(sudo fdisk -l | grep Modelo)
+DISK_NAME=$(sudo fdisk -l | grep model)
 DISK_NAME="$(echo ${DISK_NAME})"
 echo -e '\033[32;1m ==== Informacoes hardware ==== \033[m'
- 
  
 cat<<EOT
  
 Hostname       : $(hostname)
 Memoria        : ${MEM}
 Processador    : [ ${NPROC} ] ${PROC}
-Disco(s)       : $DISK
+Disco(s)       : ${DISK}
 $DISK_NAME
  
 EOT
@@ -53,15 +61,10 @@ for i in $NIC
 do
  
     IP=$(ifconfig $i | egrep -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | tail -3 | head -1)
- 
     BCAST=$(ifconfig $i | egrep -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | tail -2 | head -1)
- 
     MASK=$(ifconfig $i | egrep -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | tail -1 | head -1)
- 
     REDE=$(ip ro | egrep "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[1-3]{1,2}.*$i.*$IP" | egrep -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}")
- 
     MAC_ADDR=$(ifconfig eth0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
- 
  
 ip ro | grep -o "default equalize" > /dev/null
  
@@ -91,7 +94,3 @@ fi
 done
 DNS=$(awk '/nameserver/ {print $2}' /etc/resolv.conf | tr -s '\n' ' ')
 echo -e "DNS Servers........: $DNS"
-
-echo
- 
-echo -e '\033[32;1m ==== Informacoes rede ==== \033[m'
